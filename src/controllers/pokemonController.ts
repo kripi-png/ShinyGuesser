2d91a0f632be030d7b1a428e5b3b1c6ce1ded703
kripi-png <66375160+kripi-png@users.noreply.github.com>
2023-10-14 20:23:06 +0300

    feat: endpoint for random pokemon name and sprites

---
+++ src/controllers/pokemonController.ts
@@ -0,0 +1,45 @@
+import { RequestHandler } from 'express';
+import pokemon from 'pokemon';
+import { PokemonClient } from 'pokenode-ts';
+const pokeApi = new PokemonClient();
+
+const getRandomFromList = (list: Array<string>) => {
+	// generate a random number between 0 and list's length
+	// and return the value with the index from the array
+	const rIndex = Math.floor(Math.random() * list.length);
+	return list[rIndex];
+};
+
+const getRandomPokemon: RequestHandler = async (req, res, next) => {
+	try {
+		const pokemonName = pokemon.random();
+		const pokemonId = pokemon.getId(pokemonName);
+		// first find out if there are varieties for the pokemon (e.g. Wormadam has 3)
+		const { varieties } = await pokeApi.getPokemonSpeciesById(pokemonId);
+		const varietyNames = varieties.map((v) => v.pokemon.name);
+		// get "random" variety (e.g. wormadam-trash), even if there is only one
+		const varietyName = getRandomFromList(varietyNames);
+		const data = await pokeApi.getPokemonByName(varietyName);
+
+		res.status(200).json({
+			pokemonId,
+			pokemonName,
+			varietyName,
+			has_gender_difference: data.sprites.front_female !== null,
+			sprites: {
+				front_default: data.sprites.front_default,
+				front_shiny: data.sprites.front_shiny,
+				front_female: data.sprites.front_female,
+				front_shiny_female: data.sprites.front_shiny_female,
+			},
+		});
+	} catch (error: any) {
+		console.error(error);
+		res.status(500).json({ error: error.message });
+	}
+	next();
+};
+
+export default {
+	getRandomPokemon,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,18 @@
-import cors from 'cors';
 import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
-
 dotenv.config();
 
-const port = process.env.PORT;
+import cors from 'cors';
+import express, { Express } from 'express';
+import router from './router/index.js';
+
 const app: Express = express();
+app.set('port', process.env.PORT || 8000);
+
 app.use(cors());
+app.use(router);
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('Express + asd TypeScript Server');
-});
-
-app.get('/random', (req, res) => {
-	const r = Math.round(Math.random() * 100);
-	res.status(200).json({ random: r });
-});
-
-app.listen(port, () => {
-	console.log(`[server]: Server is running at http://localhost:${port}`);
+app.listen(app.get('port'), () => {
+	console.log(
+		`[server]: Server is running at http://localhost:${app.get('port')}`
+	);
 });
