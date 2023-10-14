5a24176f3f026f7742956cb38841eee4571f42a8
kripi-png <66375160+kripi-png@users.noreply.github.com>
2023-10-14 22:56:17 +0300

    style: format code

--- svelte/tailwind.config.ts
+++ svelte/tailwind.config.ts
@@ -1,26 +1,25 @@
+import { skeleton } from '@skeletonlabs/tw-plugin';
 import { join } from 'path';
 import type { Config } from 'tailwindcss';
-import { skeleton } from '@skeletonlabs/tw-plugin';
-
 
 const config = {
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve(
-			'@skeletonlabs/skeleton'),
+		join(
+			require.resolve('@skeletonlabs/skeleton'),
 			'../**/*.{html,js,svelte,ts}'
-		)
+		),
 	],
 	theme: {
 		extend: {},
 	},
 
 	plugins: [
-	skeleton({
-		themes: { preset: [ "skeleton" ] }
-	})
-]
+		skeleton({
+			themes: { preset: ['skeleton'] },
+		}),
+	],
 } satisfies Config;
 
 module.exports = config;
