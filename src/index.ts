87cbb26012990ffeea51dad7c3d6dc28382b1956
kripi-png <66375160+kripi-png@users.noreply.github.com>
2023-10-14 16:58:30 +0300

    feat: endpoint /random that returns a random number between 0-100

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
 
 dotenv.config();
 
-const app: Express = express();
 const port = process.env.PORT;
+const app: Express = express();
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-	res.send('Express + TypeScript Server');
+	res.send('Express + asd TypeScript Server');
+});
+
+app.get('/random', (req, res) => {
+	const r = Math.round(Math.random() * 100);
+	res.status(200).json({ random: r });
 });
 
 app.listen(port, () => {
