fa57798ba879f2c64f933ece6a27bc19cbd0f71e
kripi-png <66375160+kripi-png@users.noreply.github.com>
2023-10-14 20:26:37 +0300

    chore: remove default components and styling

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,21 +1,5 @@
 <script lang="ts">
-	import svelteLogo from './assets/svelte.svg';
-	import viteLogo from '/vite.svg';
-	import Counter from './lib/Counter.svelte';
-
-	import { onMount } from 'svelte';
-
-	const getNumber = async () => {
-		const res = await fetch('http://localhost:8000/random');
-		const { random } = await res.json();
-		return random;
-	};
 </script>
 
 <div>
-	{#await getNumber()}
-		Loading...
-	{:then number}
-		{number}
-	{/await}
 </div>
