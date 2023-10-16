b08db3423434906ec1c86cd5c98e1bd07351225b
kripi-png <66375160+kripi-png@users.noreply.github.com>
2023-10-16 17:21:30 +0300

    build: rewrite build scripts

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
 		},
 	},
 	server: {
-    proxy: {
-      '/api': 'http://localhost:8080',
-    },
-  },
+		proxy: {
+			'/api': 'http://localhost:8080',
+		},
+	},
 });
