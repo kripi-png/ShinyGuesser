86d6665dbcb75ae957f3e57aa62233b7923d4f25
kripi-png <66375160+kripi-png@users.noreply.github.com>
2023-10-16 19:26:25 +0300

    feat: display variety's name if applicable

--- vue/src/components/PokemonName.vue
+++ vue/src/components/PokemonName.vue
@@ -9,12 +9,18 @@ const { currentPokemon } = storeToRefs(store);
 <template>
 	<h2 v-if="currentPokemon">
 		<span class="pokemonId">#{{ currentPokemon.pokemonId }}</span>
+		<span v-if="currentPokemon.varietyName" class="varietyName"
+			>&nbsp;({{ currentPokemon.varietyName }})</span
+		>
 		{{ currentPokemon.pokemonName }}
 	</h2>
 	<h2 v-else>Loading...</h2>
 </template>
 
 <style scoped>
+.varietyName {
+	text-transform: capitalize;
+}
 .pokemonId {
 	font-size: 0.75em;
 }
