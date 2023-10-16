7e69ad197bf107b2c55000d2c94e404bd67d47b2
kripi-png <66375160+kripi-png@users.noreply.github.com>
2023-10-16 17:32:11 +0300

    build: include dist

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const pokemon_1 = __importDefault(require("./api/pokemon/"));
+const path = __dirname + '/views/';
+const app = (0, express_1.default)();
+app.set('port', 8080);
+app.use((0, cors_1.default)());
+app.use(express_1.default.static(path));
+app.use('/api/pokemon', pokemon_1.default);
+app.get('/', function (req, res) {
+    res.sendFile(path + 'index.html');
+});
+app.listen(app.get('port'), () => {
+    console.log(`[server]: Server is running at http://localhost:${app.get('port')}`);
+});
+//# sourceMappingURL=index.js.map
