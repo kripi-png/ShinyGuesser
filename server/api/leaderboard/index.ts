27caa4e05b98814f7ff2a43eda398c97c0563cb9
kripi-png <66375160+kripi-png@users.noreply.github.com>
2024-02-22 13:30:38 +0200

    refactor: include time in leaderboard request

--- server/api/leaderboard/index.ts
+++ server/api/leaderboard/index.ts
@@ -3,41 +3,51 @@ import { kv } from '@vercel/kv';
 
 const router = express.Router();
 
-// get :count amount of scores; default to 10
-// values are stored in increasing order in the database
-// so by default get the entries in reversed order
-// using ?reverse=true switches the order around
-router.get(
-	'/:count?',
-	async (req: Request<{ count?: number; reverse?: boolean }>, res) => {
-		const reverse = req.query.reverse === 'true' ? true : false;
-		let count = Number(req.params.count);
+// get ?count amount of scores; default to 10
+// values are stored in ascending order in the database
+// so by default get the entries in reversed order (descending)
+// using ?reverse=true switches the order around (back to ascending)
+// a little counterintuitive maybe but it is what it is
+router.get('/', async (req, res) => {
+	const reverse = req.query.reverse === 'true' ? true : false;
+	// if not set default to 10
+	const countParam = req.query.count ? req.query.count : 10;
+	try {
+		const count = Number(countParam);
 		if (isNaN(count))
 			return res.status(400).send('invalid count: needs to be a number');
-		// if not set default to 10
-		if (!count) count = 10;
+		if (count < 0)
+			return res.status(400).send('count must be a positive integer');
 
 		// initially scores are returned in a list where name and score are altered
 		// [name1, score1, name2, score2, ...]
-		const flatUserScoreList = await kv.zrange('scores', 0, count, {
-			rev: !reverse,
-			withScores: true,
+		// lower count by one because the first element is also counted,
+		// so actually it is 1 + count
+		const flatUserScoreList = await kv.zrange('scores', 0, count - 1, {
+			rev: !reverse, // method returns reversed (ascending) list by default
+			withScores: true, // also return the scores themselves and not just usernames
 		});
 
-		// convert the raw values to an array of tuples
-		// [[name1, score1], [name2, score2], ...]
-		const leaderboard: [string, number][] = [];
-		for (let index = 0; index < flatUserScoreList.length - 1; index++) {
-			const value = flatUserScoreList[index].toString();
-			const nextValue = flatUserScoreList[index + 1];
-			if (typeof value === 'string' && typeof nextValue === 'number') {
-				leaderboard.push([value, nextValue]);
-			}
+		// generate a list of objects for each user and their score and time
+		const leaderboard = [];
+		for (let i = 0; i < flatUserScoreList.length; i += 2) {
+			const user = flatUserScoreList[i]; // i = 0, 2, 4
+			const score = flatUserScoreList[i + 1]; // i+1 = 1, 3, 5
+			const time = Number((await kv.hget(`user:${user}`, 'time')) || 0);
+			leaderboard.push({ user, score, time });
 		}
 
-		return res.status(200).json(leaderboard);
+		return res.status(200).json({
+			data: leaderboard,
+			meta: {
+				total: leaderboard.length,
+			},
+		});
+	} catch (error: any) {
+		console.error(error);
+		return res.status(500).send(error.name + ': ' + error.message);
 	}
-);
+});
 
 // POST /api/leaderboard/:user?streak=number&time=number
 router.post(
@@ -79,7 +89,7 @@ router.post(
 			return res
 				.status(200)
 				.send('Time/streak was not saved: worse than previous best.');
-		} catch (error) {
+		} catch (error: any) {
 			console.error(error);
 			return res.status(500).send(error.name + ': ' + error.message);
 		}
