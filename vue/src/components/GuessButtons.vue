308b25a5068ec3106622e645e5062dad9581b6d0
kripi-png <66375160+kripi-png@users.noreply.github.com>
2024-02-23 20:23:31 +0200

    refactor: front to work with refactored endpoint

--- vue/src/components/GuessButtons.vue
+++ vue/src/components/GuessButtons.vue
@@ -24,7 +24,7 @@ const askForUsername = (): string | false => {
 	return name;
 };
 
-const handleGuess = async (correctGuess: Boolean) => {
+const handleGuess = async (correctGuess: boolean) => {
 	if (!currentPokemon || store.isLoading) return;
 
 	if (correctGuess) {
@@ -69,7 +69,7 @@ const handleGuess = async (correctGuess: Boolean) => {
 			variant="tonal"
 			color="blue-darken-4"
 			:disabled="!currentPokemon"
-			@click="handleGuess(!currentPokemon.isShiny)"
+			@click="handleGuess(!currentPokemon.sprite.isShiny)"
 		>
 			Normal
 		</v-btn>
@@ -77,7 +77,7 @@ const handleGuess = async (correctGuess: Boolean) => {
 			variant="tonal"
 			color="yellow-darken-2"
 			:disabled="!currentPokemon"
-			@click="handleGuess(currentPokemon.isShiny)"
+			@click="handleGuess(currentPokemon.sprite.isShiny)"
 		>
 			Shiny
 		</v-btn>
